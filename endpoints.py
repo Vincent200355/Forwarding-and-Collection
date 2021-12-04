899960c4b80b3a6c55ea5d16991c8e72381902ee
Vincent <s200355@student.dhbw-mannheim.de>
2021-12-04 17:07:25 +0100

    Added database type name
    + Added database type name attribute to each endpoint

--- endpoints.py
+++ endpoints.py
@@ -8,29 +8,37 @@ ENDPOINTS = {}
 
 class Endpoint():
 	
-	def __init__(self, endpointName, pollingInterval):
+	def __init__(self, endpointName, pollingInterval, databaseTypeName):
 		"""Constructs a new Endpoint with the given endpointName and
 		pollingInterval.
 		
 		The endpointName must be of type str. The name must not be None nor the
 		empty string.
 		
+		The databaseTypeName must be of type str. The databaseTypeName must not
+		be None.
+		
 		Keyword arguments:
 			endpointName -- the name for the endpoint
 			pollingInterval -- the polling interval for the endpoint, in
 			milliseconds
+			databaseTypeName -- the database name of the endpoint
 		
 		Raises:
 			RuntimeError -- if the given endpointName is not of type str
 			RuntimeError -- if the given endpointName is empty
+			RuntimeError -- if the given databaseTypeName is not of type str
 			
 		"""
 		if not isinstance(endpointName, str):
 			raise RuntimeError("endpoint name must be of type str")
 		if endpointName == "":
 			raise RuntimeError("endpoint name must not be empty")
+		if not isinstance(databaseTypeName, str):
+			raise RuntimeError("database type name be of type str")
 		self._name = endpointName
 		self._interval = pollingInterval
+		self._dbTypeName = databaseTypeName
 	
 	def name(self):
 		"""Returns the name of this endpoint.
@@ -53,6 +61,17 @@ class Endpoint():
 			an int which represents the polling interval, in milliseconds
 		"""
 		return self._interval
+	
+	def databaseName(self):
+		"""Returns the database name of the endpoint.
+		
+		Keyword arguments:
+			none
+		
+		Returns:
+			an str used in the database to represent the endpoint
+		"""
+		return self._dbTypeName
 	
 	def __eq__(self, other):
 		"""Returns weather other is equal to this endpoint
@@ -81,7 +100,7 @@ class Endpoint():
 		"""
 		return hash(self._name)
 
-def registerEndpoint(name, interval):
+def registerEndpoint(name, interval, databaseTypeName):
 	"""Registers a new endpoint with the given name and registers it to the
 	ENDPOINTS dictionary.
 	
@@ -91,23 +110,28 @@ def registerEndpoint(name, interval):
 	The name must be of type str. The name must not be None nor the empty
 	string.
 	
+	The databaseTypeName must be of type str. The databaseTypeName must not be
+	None.
+	
 	Keyword arguments:
 		name -- the name for the endpoint
 		interval -- the polling interval for the endpoint, in milliseconds
+		databaseTypeName -- the database name of the endpoint
 	
 	Raises:
 		RuntimeError -- if the given name is not of type str
 		RuntimeError -- if the given name is empty
 		RuntimeError -- if there is a registered endpoint with the given name
+		RuntimeError -- if the given databaseTypeName is not of type str
 		
 	"""
-	e = Endpoint(name, interval)
+	e = Endpoint(name, interval, databaseTypeName)
 	if e.name() in ENDPOINTS:
 		raise RuntimeError("an endpoint named \"" + e.name() + "\" is already present")
 	ENDPOINTS[e.name()] = e
 
 # Register default endpoints
-registerEndpoint("flightplans", 30000)
-registerEndpoint("terminal", 15000)
-registerEndpoint("radar", 20000)
-registerEndpoint("it", 10000)
+registerEndpoint("flightplans", 30000, "FLIGHTPLAN")
+registerEndpoint("terminal", 15000, "TERMINAL")
+registerEndpoint("radar", 20000, "RADAR")
+registerEndpoint("it", 10000, "IT")
